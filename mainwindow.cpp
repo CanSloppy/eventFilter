4aadc66b8607166087885eac8f7a121f7b274728
CanSloppy <1452034610@qq.com>
2024-05-07 17:50:32 +0800

    file add

---
+++ mainwindow.cpp
@@ -0,0 +1,52 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->installEventFilter(this);                 //主页面设置事件过滤器
+    ui->plainTextEdit->installEventFilter(this);    //文本框设置事件过滤器
+    ui->plainTextEdit->setEnabled(false);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *event)
+{
+    Q_UNUSED(event);
+    ui->plainTextEdit->appendPlainText("鼠标点击");
+}
+
+void MainWindow::mouseDoubleClickEvent(QMouseEvent *event)
+{
+    Q_UNUSED(event);
+    ui->plainTextEdit->appendPlainText("鼠标双击");     //此句话被过滤掉了
+}
+
+bool MainWindow::eventFilter(QObject *obj, QEvent *event)
+{
+    if(obj == ui->plainTextEdit)
+    {
+       if(event->type() == QEvent::MouseButtonDblClick)
+       {
+           ui->plainTextEdit->appendPlainText("过滤plainTextEdit双击事件");
+           return true;
+       }
+    }
+    if(obj == this)
+    {
+       if(event->type() == QEvent::MouseButtonDblClick)
+       {
+           ui->plainTextEdit->appendPlainText("过滤主页面双击事件");
+           return true;
+       }
+    }
+    return QMainWindow::eventFilter(obj,event);
+}
+
---
+++ mainwindow.h
@@ -0,0 +1,24 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    virtual void mousePressEvent(QMouseEvent *event);
+    virtual void mouseDoubleClickEvent(QMouseEvent *event);
+    virtual bool eventFilter(QObject *obj, QEvent *event);
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
